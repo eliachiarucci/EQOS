b336fb7092b562b1e116f4297becabe10739c5e7
eliachiarucci <elia.chiarucci@gmail.com>
2026-02-22 18:11:07 +0100

    feat: fix router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,11 @@ import './assets/main.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createMemoryHistory } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-const router = createRouter({ routeTree })
+const memoryHistory = createMemoryHistory({ initialEntries: ['/'] })
+const router = createRouter({ routeTree, history: memoryHistory })
 
 declare module '@tanstack/react-router' {
   interface Register {
